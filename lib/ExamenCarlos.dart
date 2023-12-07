55e9fe433f98c9eee19a97463a0026aa835b5180
“Carlos <carloscasta.perea@gmail.com>
2023-12-08 00:06:33 +0100

    PMDM: P2) Creación del home (las rutas ya las implanté al crear el diseño del login y el register).

--- lib/ExamenCarlos.dart
+++ lib/ExamenCarlos.dart
@@ -1,8 +1,8 @@
+import 'package:examen_pmdm_psp_carlos_castanedo_perea/Main/HomeView.dart';
 import 'package:examen_pmdm_psp_carlos_castanedo_perea/OnBoarding/CambiarPasswordView.dart';
 import 'package:examen_pmdm_psp_carlos_castanedo_perea/OnBoarding/RegisterView.dart';
 import 'package:examen_pmdm_psp_carlos_castanedo_perea/OnBoarding/SplashView.dart';
 import 'package:flutter/material.dart';
-
 import 'OnBoarding/LoginView.dart';
 
 class ExamenCarlos extends StatelessWidget{
@@ -15,6 +15,7 @@ class ExamenCarlos extends StatelessWidget{
       '/registerview': (context) => RegisterView(),
       '/cambiarpasswordview': (context) => CambiarPasswordView(),
       '/splashview': (context) => SplashView(),
+      '/homeview': (context) => HomeView(),
     },
         initialRoute: '/loginview',
     );
