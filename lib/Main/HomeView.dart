adb9f910fc3863b58451320e70fb25243a67cd52
“Carlos <carloscasta.perea@gmail.com>
2023-12-08 00:06:50 +0100

    PMDM: P2) Creación del home (las rutas ya las implanté al crear el diseño del login y el register).

---
+++ lib/Main/HomeView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text("En mantenimiento");
+  }
+}
